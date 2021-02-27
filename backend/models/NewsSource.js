aeb78271c6790239f57ec77b942fcc206a49f06e
ryzhak <ryzhak.vladimir@gmail.com>
2021-02-27 21:59:04 +0300

    feat: save news from championat and soccernews

--- backend/models/NewsSource.js
+++ backend/models/NewsSource.js
@@ -1,3 +1,4 @@
+const moment = require('moment');
 const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
@@ -16,6 +17,47 @@ const modelSchema = new Schema({
 		required: true
 	}
 });
+
+/**
+ * Converts news item from RSS XML feed to news DB model
+ * @param {string} newsSourceName news source name 
+ * @param {string} newsSourceId news source mongo id
+ * @param {Object} xmlItem item data from RSS XML feed
+ * @return {Object} data prepared for news model 
+ */
+modelSchema.statics.getNewsDataFromXmlItem = (newsSourceName, newsSourceId, xmlItem) => {
+	// empty news data model
+	let newsData = {
+		title: null,
+		url: null,
+		published_at: null,
+		guid: null,
+		description: null,
+		tags: [],
+		source_id: null
+	};
+	// championat.com
+	if (newsSourceName === 'championat.com') {
+		newsData.title = xmlItem.title;
+		newsData.url = xmlItem.link;
+		newsData.published_at = moment(xmlItem.pubDate).unix();
+		newsData.guid = xmlItem.guid;
+		newsData.description = xmlItem.content;
+		newsData.tags = xmlItem.tags ? xmlItem.tags.tag.map(tag => tag._) : [];
+		newsData.source_id = newsSourceId
+	}
+	// soccernews.com
+	if (newsSourceName === 'soccernews.com') {
+		newsData.title = xmlItem.title;
+		newsData.url = xmlItem.link;
+		newsData.published_at = moment(xmlItem.pubDate).unix();
+		newsData.guid = xmlItem.guid;
+		newsData.description = xmlItem.content;
+		newsData.tags = xmlItem.categories;
+		newsData.source_id = newsSourceId
+	}
+	return newsData;
+};
 
 const NewsSource = mongoose.model('NewsSource', modelSchema);
 
--- backend/scripts/parse-and-notify-news.js
+++ backend/scripts/parse-and-notify-news.js
@@ -1,7 +1,19 @@
 require('dotenv').config();
 
+const Parser = require('rss-parser');
 const db = require('../lib/db');
+const News = require('../models/News');
 const NewsSource = require('../models/NewsSource');
+
+// prepare XML parser
+const parser = new Parser({
+	customFields: {
+		item: [
+			// championat.com custom xml tags
+			'tags'
+		]
+	}
+});
 
 /**
  * Main function
@@ -20,8 +32,19 @@ async function run() {
 		// for all news sources
 		const newsSources = await NewsSource.find();
 		for (let newsSource of newsSources) {
-			// TODO: get feed
-			console.log(newsSource);
+			// get feed
+			const feed = await parser.parseURL(newsSource.rss_url);
+			// for all feed items
+			for (let feedItem of feed.items) {
+				// convert feed item to news data
+				const newsData = NewsSource.getNewsDataFromXmlItem(newsSource.name, newsSource._id, feedItem);
+				// if news model does not exist then save it
+				let newsModel = await News.findOne({url: newsData.url});
+				if (!newsModel) {
+					newsModel = new News(newsData);
+					await newsModel.save();
+				}
+			}
 		}
 	} catch (err) {
 		console.log('===Error===');
