3033cb6f5ddb0f006d40fb520a01f1536f69ec1f
ryzhak <ryzhak.vladimir@gmail.com>
2021-04-10 01:53:45 +0300

    feat: add login page layout

--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -1,3 +1,6 @@
+import { Button } from 'primereact/button';
+import { Card } from 'primereact/card';
+import { InputText } from 'primereact/inputtext';
 import React from 'react';
 
 /**
@@ -6,7 +9,30 @@ import React from 'react';
 export default class LoginPage extends React.PureComponent {
     render() {
 		return (
-			<div>login</div>
+			<div className="app-w-100 app-h-100">
+				<div className="p-grid p-justify-center p-align-center app-w-100 app-h-100">
+					<div className="p-col-4">
+						<Card>
+							<div className="p-grid">
+								<div className="p-col">
+									<img src="/assets/img/ibra.jpg" />
+								</div>
+								<div className="p-col">
+									<h1>Balleilo</h1>
+									<h2>Sports Media CMS</h2>
+									<div className="p-fluid">
+										<div className="p-field">
+											<label htmlFor="api_balleilo_key">API Balleilo key</label>
+											<InputText id="api_balleilo_key" type="text" placeholder="API Balleilo key" />
+										</div>
+										<Button label="Login" />
+									</div>
+								</div>
+							</div>
+						</Card>
+					</div>
+				</div>
+			</div>
 		); 
     }
 }
