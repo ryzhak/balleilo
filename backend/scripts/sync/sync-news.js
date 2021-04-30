6ab159769168e6d473818e491eed1dfd2b2c5acc
ryzhak <ryzhak.vladimir@gmail.com>
2021-04-30 13:45:13 +0300

    feat: add sync news script

--- backend/scripts/sync/sync-news.js
+++ backend/scripts/sync/sync-news.js
@@ -1,15 +1,11 @@
 // setup path for env file in order for script to work in production
-require('dotenv').config({ path: `${__dirname}/../../.env` });
+require('dotenv').config({ path: `${__dirname}/../../../.env` });
 
 const moment = require('moment');
-const TelegramBot = require('node-telegram-bot-api');
 const Parser = require('rss-parser');
-const db = require('../lib/db');
-const News = require('../models/app/News');
-const NewsSource = require('../models/app/NewsSource');
-
-// prepare telegram bot for posting news
-const telegramBot = new TelegramBot(process.env.TELEGRAM_BOT_API_TOKEN);
+const db = require('../../lib/db');
+const News = require('../../models/app/News');
+const NewsSource = require('../../models/app/NewsSource');
 
 // prepare XML parser
 const parser = new Parser({
@@ -50,17 +46,6 @@ async function run() {
 				}
 			}
 		}
-
-		// post freshly parsed news to telegram channel
-		const newsToPublishItems = await News.find({created_at: {$gte: scriptStartedAt}});
-		// NOTICE: telegram bot can not send more than 20 messages per minute in the same group(channel) due to API rate limit
-		for (let i = 0; i < newsToPublishItems.length; i++) {
-			// publish news item to telegram channel
-			await telegramBot.sendMessage('@fckrd1', `${newsToPublishItems[i].title}\n\n${newsToPublishItems[i].description}\n\n${newsToPublishItems[i].url}`);
-			// wait for 1 minute every 15 news items
-			if ((i + 1) % 15 == 0) await new Promise(resolve => setTimeout(resolve, 60 * 1000));
-		}
-
 	} catch (err) {
 		console.log('===Error===');
 		console.log(err);
